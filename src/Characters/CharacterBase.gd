51a14b86f88c128c07b67488fc3f2a7fbcd4a900
TheBeeEmperor <104157076+TheBeeEmperor@users.noreply.github.com>
2022-09-25 21:03:30 +0300

    Add files via upload

--- src/Characters/CharacterBase.gd
+++ src/Characters/CharacterBase.gd
@@ -4,6 +4,6 @@ class_name CharacterBase
 
 const FLOOR_NORMAL = Vector2.UP
 
-export var speed: = Vector2(300.0, 300.0)
+export var speed = Vector2(300.0, 300.0)
 
-var _velocity: = Vector2.ZERO
+var _velocity = Vector2.ZERO
--- src/Characters/Player.gd
+++ src/Characters/Player.gd
@@ -1,14 +1,21 @@
 extends CharacterBase
 
+var bullet_speed = 2000
+var bullet = preload("res://src/Objects/TestBullet.tscn")
+
 func _physics_process(delta):
-	var direction: = get_direction() 
+	var direction = get_direction() 
 	_velocity = calculate_move_velocity(_velocity, direction, speed)
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	_velocity = move_and_slide(_velocity)
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("shoot"):
+		fire()
 
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - 	Input.get_action_strength("move_left"),
-		Input.get_action_strength("move_up") - 	Input.get_action_strength("move_down")
+		Input.get_action_strength("move_down") - 	Input.get_action_strength("move_up")
 	)
 
 func calculate_move_velocity(
@@ -16,6 +23,21 @@ func calculate_move_velocity(
 	direction: Vector2,
 	speed: Vector2
 ) -> Vector2:
-	var out: = linear_velocity
+	var out = linear_velocity
 	out.x = speed.x * direction.x
+	out.y = speed.y * direction.y
 	return out
+
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_instance)
+
+func kill():
+	get_tree().reload_current_scene()
+
+func _on_Area2D_body_entered(body):
+	if "Enemy" in body.name:
+		kill()
